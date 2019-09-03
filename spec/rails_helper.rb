d8d4021c287c2e3db9139f4a367693cc6f589a76
Daria Lykova <daria.lykova@gmail.com>
2019-09-03 17:45:26 +0200

    Includes coverage in .gitignore file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -8,7 +8,6 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'spec_helper'
 require 'rspec/rails'
 
-
 ActiveRecord::Migration.maintain_test_schema!
 
 Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
