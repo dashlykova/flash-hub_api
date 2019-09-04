02e5ad16e488c60a8d40918233476a1e18a8e6f2
oliverochman <oliver.ochman@hotmail.com>
2019-09-04 15:03:14 +0200

    add max amount of flash_cards to be displayed on index action

--- app/controllers/api/flash_cards_controller.rb
+++ app/controllers/api/flash_cards_controller.rb
@@ -1,6 +1,8 @@
 class Api::FlashCardsController < ApplicationController
+  MAX_AMOUNT_OF_FLASH_CARDS_TO_DISPLAY = 10
+
   def index
-    flash_cards = FlashCard.all
+    flash_cards = FlashCard.first(MAX_AMOUNT_OF_FLASH_CARDS_TO_DISPLAY)
     render json: flash_cards
   end
 end
