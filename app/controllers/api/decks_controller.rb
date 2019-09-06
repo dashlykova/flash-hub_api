4727dda13e3ca7f106f2754cfaf84818a48489c1
oliverochman <oliver.ochman@hotmail.com>
2019-09-06 15:53:07 +0200

    add more error handlers to index action

--- app/controllers/api/decks_controller.rb
+++ app/controllers/api/decks_controller.rb
@@ -1,12 +1,29 @@
 class Api::DecksController < ApplicationController
   def index
-    if params[:category]      
-      decks = Deck.where(category: params[:category]).page(params[:page] || 1).per_page(1)
-      render json: decks, each_serializer: Decks::IndexSerializer,  meta: meta_attributes(decks)  
-    else
-      decks = Deck.all.page(params[:page] || 1).per_page(1)
-      render json: decks, each_serializer: Decks::IndexSerializer,  meta: meta_attributes(decks)  
+    begin
+      if params[:category]      
+        @decks = Deck.where(category: params[:category]).page(params[:page] || 1).per_page(1)
+      else
+        @decks = Deck.all.page(params[:page] || 1).per_page(1)
+      end
+  
+      unless @decks == []
+        render_deck_success
+      else
+        render_deck_error('There is no available decks on this page')
+      end
+    rescue
+      render_deck_error('Something went wrong')
     end
-    
+  end
+
+  private 
+
+  def render_deck_error(error_message)
+    render json: { error: error_message }, status: 422
+  end
+
+  def render_deck_success
+    render json: @decks, each_serializer: Decks::IndexSerializer, meta: meta_attributes(@decks)  
   end
 end
