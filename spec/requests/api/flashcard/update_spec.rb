67ede717309a721c9a7dba480eec1a890becb547
oliverochman <oliver.ochman@hotmail.com>
2019-09-06 10:55:20 +0200

    add sad paths to req spec

--- spec/requests/api/flashcard/update_spec.rb
+++ spec/requests/api/flashcard/update_spec.rb
@@ -2,21 +2,58 @@ RSpec.describe Api::FlashcardsController, type: :request do
   let(:headers) { { HTTP_ACCEPT: 'application/json' } }
   let(:flashcard) { create(:flashcard) }
   describe "PUT /api/flashcards/:id" do
-
-    before do
-      put "/api/flashcards/"+"#{flashcard.id}", 
-      params: {
-        status: 'green'
-      },
-      headers: headers
+    
+    describe 'sucessfully updates' do
+      before do
+        put "/api/flashcards/"+"#{flashcard.id}", 
+        params: {
+          status: 'green'
+        },
+        headers: headers
+      end
+  
+      it "returns 200 response" do
+        expect(response.status).to eq 200
+      end
+  
+      it "sets status for the correct flashcard" do
+        flashcard.reload
+        expect(flashcard.status).to eq 'green'
+      end
     end
 
-    it "returns 200 response" do
-      expect(response.status).to eq 200
+    describe 'no status param' do
+      before do
+        put "/api/flashcards/"+"#{flashcard.id}", 
+        headers: headers
+      end
+  
+      it "returns 422 response" do
+        expect(response.status).to eq 422
+      end
+  
+      it "returns error message" do        
+        expect(json_response['error']).to eq 'No status was sent'
+      end
     end
 
-    it "sets status for the correct flashcard" do
-      expect(flashcard.status).to eq 'green'
+    describe 'wrong status param' do
+      before do
+        put "/api/flashcards/"+"#{flashcard.id}", 
+        params: {
+          status: 'purple'
+        },
+        headers: headers
+      end
+  
+      it "returns 422 response" do
+        expect(response.status).to eq 422
+      end
+  
+      it "returns error message" do          
+        expect(json_response['error']).to eq 'Incorrect status'
+      end
+
     end
   end
 end
