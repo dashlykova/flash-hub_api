a23dc29129a60c1249fab5878195000fb5b2af1a
oliverochman <oliver.ochman@hotmail.com>
2019-09-06 15:07:38 +0200

    add paginator

--- app/controllers/api/decks_controller.rb
+++ app/controllers/api/decks_controller.rb
@@ -1,9 +1,12 @@
 class Api::DecksController < ApplicationController
   def index
-    # if params[:category]
-    #   @deck = Deck.where(category: params[:category]).first
-    # else
-    #   @deck = Deck.last
-    # end
+    if params[:category]      
+      decks = Deck.where(category: params[:category]).page(params[:page] || 1).per_page(1)
+      render json: decks, each_serializer: Decks::IndexSerializer,  meta: meta_attributes(decks)  
+    else
+      decks = Deck.all.page(params[:page] || 1).per_page(1)
+      render json: decks, each_serializer: Decks::IndexSerializer,  meta: meta_attributes(decks)  
+    end
+    
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::API
+
+  protected  
+  
+  def meta_attributes(resource, extra_meta = {})    
+    {
+      current_page: resource.current_page,
+      next_page: resource.next_page,
+      prev_page: resource.previous_page,
+      total_pages: resource.total_pages,
+      total_count: resource.total_entries
+    }.merge(extra_meta)
+  end
 end
