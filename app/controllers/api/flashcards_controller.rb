64346b34749ce15690a0f601daedf33d1aa11d85
oliverochman <oliver.ochman@hotmail.com>
2019-09-06 10:55:41 +0200

    add error handlers to update action

--- app/controllers/api/flashcards_controller.rb
+++ app/controllers/api/flashcards_controller.rb
@@ -8,14 +8,23 @@ class Api::FlashcardsController < ApplicationController
 
   def update
     flashcard = Flashcard.find_by(id: params[:id])
-    binding.pry
-    
-    flashcard.update_attributes(flashcard_params)
-    
-    
+    if params[:status]
+      begin 
+        flashcard.update_attribute(:status, params[:status])
+        render json: { message: 'Successfully added status' }
+      rescue
+        render_update_error('Incorrect status')
+      end
+    else
+      render_update_error('No status was sent')
+    end  
   end
 
   private
+
+  def render_update_error(error_message)
+    render json: { error: error_message }, status: 422
+  end
 
   def flashcard_params
     params.permit(:status)
