6c26d2af4e6bb53c1075dd11acbb2e846523efec
oliverochman <oliver.ochman@hotmail.com>
2019-09-06 13:37:41 +0200

    add decks controller

---
+++ app/controllers/api/decks_controller.rb
@@ -0,0 +1,9 @@
+class Api::DecksController < ApplicationController
+  def index
+    # if params[:category]
+    #   @deck = Deck.where(category: params[:category]).first
+    # else
+    #   @deck = Deck.last
+    # end
+  end
+end
