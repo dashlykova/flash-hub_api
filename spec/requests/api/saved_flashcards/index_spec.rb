c0c394f90ae6e2c75c1f2d55d72c50db73cc5df3
oliverochman <oliver.ochman@hotmail.com>
2019-09-11 19:56:27 +0200

    add req spec for saved_flashcards index

---
+++ spec/requests/api/saved_flashcards/index_spec.rb
@@ -0,0 +1,46 @@
+RSpec.describe Api::FlashcardsController, type: :request do
+  let(:user) { create(:user) }
+  let(:credentials) { user.create_new_auth_token }
+  let(:headers) { { HTTP_ACCEPT: 'application/json' }.merge!(credentials) }
+  let(:red_saved_flashcards) { 7.times { create(:saved_flashcard, status: "red", user: user) } }
+  let(:yellow_saved_flashcards) { 10.times { create(:saved_flashcard, status: "yellow", user: user) } }
+
+  describe "GET /api/saved_flashcards" do
+
+    describe 'sucessfully gets users saved flashcards [red ones]' do
+      before do        
+        get "/api/saved_flashcards", 
+        params: {
+          status: 'red'
+        },
+        headers: headers
+      end
+  
+      it "returns 200 response" do
+        expect(response.status).to eq 200
+      end
+  
+      it "sets status for the correct flashcard" do
+        expect(json_response['saved_flashcards'].count).to eq 7
+      end
+    end
+
+    describe 'sucessfully gets users saved flashcards [red & yellow ones]' do
+      before do        
+        get "/api/saved_flashcards", 
+        params: {
+          status: 'yellow'
+        },
+        headers: headers
+      end
+  
+      it "returns 200 response" do
+        expect(response.status).to eq 200
+      end
+  
+      it "sets status for the correct flashcard" do
+        expect(json_response['saved_flashcards'].count).to eq 17
+      end
+    end
+  end
+end
