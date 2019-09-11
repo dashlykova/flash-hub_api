24e54be03b50a763ffab8628667c7391b7e3c953
oliverochman <oliver.ochman@hotmail.com>
2019-09-11 19:58:23 +0200

    setup route for saved flashcard index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :flashcards, only: [:update]
     resources :decks, only: [:index]
+    resources :saved_flashcards, only: [:index]
     mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   end
 end
