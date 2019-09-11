c5daed14f0ab193b27b185ab7b0344f83b4dcc09
oliverochman <oliver.ochman@hotmail.com>
2019-09-11 14:50:46 +0200

    add req specs for login and registration

---
+++ spec/requests/api/token_auth/registrations_spec.rb
@@ -0,0 +1,63 @@
+RSpec.describe 'User Registration', type: :request do
+  let(:headers) { { HTTP_ACCEPT: 'application/json' } }
+
+  context 'with valid credentials' do
+    it 'returns a user and a token' do
+      post '/api/auth', 
+      params: { 
+        email: 'julie@dash.se',
+        password: 'password',
+        password_confirmation: 'password'
+      }, 
+      headers: headers
+
+      expect(json_response['status']).to eq 'success'
+      expect(response.status).to eq 200
+    end
+  end
+
+  context 'returns an error when user tries to submit' do
+    it 'a non-matching password confirmation' do
+      post '/api/auth', params: { 
+        email: 'julie@dash.se',
+        password: 'password',
+        password_confirmation: 'bad_password'
+      }, 
+      headers: headers
+
+      expect(json_response['errors']['password_confirmation']).to eq ["doesn't match Password"]
+      expect(response.status).to eq 422
+    end
+
+    it 'an invalid email address' do
+      post '/api/auth', params: { 
+        email: 'julie@dash',
+        password: 'password',
+        password_confirmation: 'password'
+      }, 
+      headers: headers
+
+      expect(json_response['errors']['email']).to eq ['is not an email']
+      expect(response.status).to eq 422
+    end
+
+    it 'an already registered email address' do
+      FactoryBot.create(
+        :user, email: 'julie@dash.se',
+        password: 'password',
+        password_confirmation: 'password'
+      )
+
+      post '/api/auth', 
+      params: { 
+        email: 'julie@dash.se',
+        password: 'password',
+        password_confirmation: 'password'
+      }, 
+      headers: headers
+
+      expect(json_response['errors']['email']).to eq ['has already been taken']
+      expect(response.status).to eq 422
+    end
+  end
+end 
