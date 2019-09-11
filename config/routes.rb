dea0ad8cf4cc220c4e6a36e81f7a0aa2123515a6
oliverochman <oliver.ochman@hotmail.com>
2019-09-11 14:51:37 +0200

    setup routes and devise token auth initializer

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     resources :flashcards, only: [:update]
     resources :decks, only: [:index]
+    mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
   end
 end
