1dc321253ba20769cc1b00bcae782f427612516b
oliverochman <oliver.ochman@hotmail.com>
2019-09-12 21:37:16 +0200

    fix creation of saved flashcard
    Co-authored-by: julieahlquist <julieahlquist@hotmail.com>
    Co-authored-by: dashlykova <daria.lykova@gmail.com>

--- app/controllers/api/saved_flashcards_controller.rb
+++ app/controllers/api/saved_flashcards_controller.rb
@@ -21,7 +21,22 @@ class Api::SavedFlashcardsController < ApplicationController
     end   
   end
 
+  def create    
+    saved_flashcard = current_api_user.saved_flashcards.create(saved_flashcard_params)
+    
+    if saved_flashcard.persisted?
+      render json: {message: 'Successfully saved the flashcard'}
+    else
+      render_saved_flashcard_error(saved_flashcard.errors.full_messages.to_sentence)
+    end
+    
+  end
+
   private
+
+  def saved_flashcard_params
+    params.permit(:status, :flashcard_id)
+  end
 
   def render_saved_flashcard_error(error_message)    
     render json: { error: error_message }, status: 400
